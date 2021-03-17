40a48eb4deeba300a912b549cc206c8d136591ed
Mariana Soto <a01702593@itesm.mx>
2021-03-16 23:38:39 -0600

    Comprobar inputs registrar proyecto

--- Proyecto/controller/proyectosController.js
+++ Proyecto/controller/proyectosController.js
@@ -2,7 +2,10 @@ const Proyecto = require('../models/proyecto');
 
 
 exports.getNuevaIteracion = (request, response) => {
-    response.render('crearIteracion');
+    response.render('crearIteracion',{
+        alerta : request.session.alerta,
+        csrfToken: request.csrfToken(),
+    });
 }
 
 exports.getNuevoProyecto = (request, response) => {
@@ -11,6 +14,7 @@ exports.getNuevoProyecto = (request, response) => {
         module_ = "nueva-iteracion";
     }
     response.render('crearProyecto',{
+        error: request.session.error,
         module_ : module_,
         csrfToken: request.csrfToken(),
     });
@@ -21,12 +25,25 @@ exports.postNuevoProyecto = (request, response) => {
     if(module_ == "-nueva-iteracion"){
         module_ = "nueva-iteracion";
     }
-    response.redirect(module_);
 
     request.session.error = "";
     const nombre_proyecto = request.body.nombre;
     const descripcion = request.body.descripcion;
     const departamento = request.body.departamento;
+
+    if(!nombre_proyecto && !departamento){
+        request.session.error = "El nombre y el departamento no han sido seleccionados.";
+        response.redirect('/proyectos/nuevo-proyecto' + module_);
+    }
+    else if(!nombre_proyecto){
+        request.session.error = "El nombre está vacío";
+        response.redirect('/proyectos/nuevo-proyecto' + module_);
+    }
+    else if(!departamento){
+        request.session.error = "No se ha seleccionado el departamento";
+        response.redirect('/proyectos/nuevo-proyecto' + module_);
+    }
+
     Proyecto.fetchOne(nombre_proyecto)
         .then(([rows, fieldData]) => {
             if (rows.length < 1) {
@@ -36,6 +53,8 @@ exports.postNuevoProyecto = (request, response) => {
                     .then(([id_proyecto, fieldData]) =>{
                         Proyecto.saveProyectoDepto(departamento, id_proyecto[0].id_proyecto)
                             .then(() => {
+                                request.session.alerta = "Proyecto creado exitosamente";
+                                response.redirect('/proyectos/' + module_);
                             })
                             .catch(err => {
                                 console.log(err);
@@ -47,7 +66,7 @@ exports.postNuevoProyecto = (request, response) => {
             }
             else {
                 request.session.error = "Ya hay un proyecto con ese nombre";
-                response.redirect('/proyectos/nuevo-proyecto');
+                response.redirect('/proyectos/nuevo-proyecto' + module_);
             }
         })
         .catch(err => {
