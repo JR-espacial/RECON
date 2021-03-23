7ddf17aaaca2d6bd243714474fecaaa0dd9337f7
JR-espacial <jorge_alan_2001@hotmail.com>
2021-03-23 15:09:13 -0600

    buscar home

--- Proyecto/controller/proyectosController.js
+++ Proyecto/controller/proyectosController.js
@@ -21,54 +21,41 @@ exports.postNuevoProyecto = (request, response) => {
     const nombre_proyecto = request.body.nombre;
     const descripcion = request.body.descripcion;
     const departamento = request.body.departamento;
-
-    if(!nombre_proyecto && !departamento){
-        request.session.error = "El nombre y el departamento no han sido seleccionados.";
-        response.redirect('/proyectos/nuevo-proyecto');
-    }
-    else if(!nombre_proyecto){
-        request.session.error = "El nombre está vacío";
-        response.redirect('/proyectos/nuevo-proyecto');
-    }
-    else if(!departamento){
-        request.session.error = "No se ha seleccionado el departamento";
-        response.redirect('/proyectos/nuevo-proyecto');
-    }
-    else{
-        Proyecto.fetchOne(nombre_proyecto)
-            .then(([rows, fieldData]) => {
-                if (rows.length < 1) {
-                    let proyecto = new Proyecto(nombre_proyecto, descripcion, departamento);
-                    proyecto.saveProyecto()
-                    .then(() => {
-                        Proyecto.fetchOne(nombre_proyecto)
-                        .then(([rows2, fieldData]) =>{
-                            Proyecto.saveProyectoDepto(departamento, rows2[0].id_proyecto)
-                                .then(() => {
-                                    request.session.alerta = "Proyecto creado exitosamente";
-                                    response.redirect(last);
-                                })
-                                .catch(err => {
-                                    console.log(err);
-                                });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
+  
+    Proyecto.fetchOne(nombre_proyecto)
+        .then(([rows, fieldData]) => {
+            if (rows.length < 1) {
+                let proyecto = new Proyecto(nombre_proyecto, descripcion, departamento);
+                proyecto.saveProyecto()
+                .then(() => {
+                    Proyecto.fetchOne(nombre_proyecto)
+                    .then(([rows2, fieldData]) =>{
+                        Proyecto.saveProyectoDepto(departamento, rows2[0].id_proyecto)
+                            .then(() => {
+                                request.session.alerta = "Proyecto creado exitosamente";
+                                response.redirect(last);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            });
                     })
                     .catch(err => {
                         console.log(err);
                     });
-                }
-                else {
-                    request.session.error = "Ya hay un proyecto con ese nombre";
-                    response.redirect('/proyectos/nuevo-proyecto');
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            }
+            else {
+                request.session.error = "Ya hay un proyecto con ese nombre";
+                response.redirect('/proyectos/nuevo-proyecto');
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
 }
 
 exports.getResumenProyecto = (request, response) =>{
