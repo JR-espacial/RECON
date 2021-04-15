d53fec3fb57de836aca4a26631740160d1a8ee07
Mariana Soto <a01702593@itesm.mx>
2021-04-14 21:42:07 -0500

    Tabla costos dinámica

--- Proyecto/controller/avanceIteracionController.js
+++ Proyecto/controller/avanceIteracionController.js
@@ -1,50 +1,43 @@
 const Proyecto = require('../models/proyecto');
 const Iteracion = require('../models/iteracion');
 const Capacidad_Equipo = require('../models/capacidad_equipo');
+const Entrega = require('../models/entrega');
 
 
 
-exports.getAvanceProyecto = (request, response) => {
+exports.getAvanceProyecto = async function (request, response) {
     request.session.navegacion = 2;
-    Iteracion.fetchOneID(request.session.idIteracion)
-    .then(([rows, fieldData]) => {
-        Capacidad_Equipo.fetchOne(rows[0].id_capacidad)
-        .then(([rows2, fieldData]) => {
-            let total_horas_real;
-            let date1 = new Date(rows[0].fecha_inicio);
-            let date2 = new Date(rows[0].fecha_fin);
-            let diffTime = Math.abs(date2 - date1);
-            let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    let iteracion = await Iteracion.fetchOneID(request.session.idIteracion)
+    let capacidad = await Capacidad_Equipo.fetchOne(iteracion[0][0].id_capacidad)
+    
+    let total_horas_real;
+    let date1 = new Date(iteracion[0][0].fecha_inicio);
+    let date2 = new Date(iteracion[0][0].fecha_fin);
+    let diffTime = Math.abs(date2 - date1);
+    let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 
+    if(iteracion[0][0].total_min_real){
+        total_horas_real= (iteracion[0][0].total_min_real/60).toFixed(2);
+    }
+    else{
+        total_horas_real = "Sin registrar";
+    }
+    let velocidad_deseada = (total_horas_real/diffDays).toFixed(2);
 
-            if(rows[0].total_min_real){
-                total_horas_real= (rows[0].total_min_real/60).toFixed(2);
-            }
-            else{
-                total_horas_real = "Sin registrar";
-            }
-            let velocidad_deseada = (total_horas_real/diffDays).toFixed(2);
+    let costos = await Entrega.fetchCostosDiarios(request.session.idIteracion);
 
-            
+    response.render('avanceProyecto', {
+        navegacion : request.session.navegacion,
+        proyecto_actual : request.session.nombreProyecto,
+        user: request.session.usuario,
+        iteracion: iteracion[0][0],
+        capacidad: capacidad[0][0],
+        dias_totales: diffDays,
+        horas_planeadas: total_horas_real,
+        velocidad_deseada: velocidad_deseada,
+        costos: costos[0],
+        title: "Avance del Proyecto",
+        csrfToken: request.csrfToken()
+    });
 
-            response.render('avanceProyecto', {
-                navegacion : request.session.navegacion,
-                proyecto_actual : request.session.nombreProyecto,
-                user: request.session.usuario,
-                iteracion: rows[0],
-                capacidad: rows2[0],
-                dias_totales: diffDays,
-                horas_planeadas: total_horas_real,
-                velocidad_deseada: velocidad_deseada,
-                title: "Avance del Proyecto",
-                csrfToken: request.csrfToken()
-            });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    }); 
 }
