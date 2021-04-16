7d0c5656c14aab3b5f4e6ea6421136bc7dcdd4b8
Felipe <feliyepez2001@gmail.com>
2021-04-16 18:55:40 -0500

    Modificar Capacidad Equipo Grupal
    Co-authored-by: AndreaPineiro <AndreaPineiro@users.noreply.github.com>

--- Proyecto/controller/capacidadController.js
+++ Proyecto/controller/capacidadController.js
@@ -4,6 +4,7 @@ const capacidad_equipo = require('../models/capacidad_equipo');
 exports.getCapacidadEquipo = (request, response) =>{
     const id_iteracion = request.session.idIteracion;
     let alerta = request.session.alerta;
+    request.session.alerta = "";
     capacidad_equipo.fetchCapacidadEmpleados(id_iteracion)
         .then(([rows, fieldData]) => {
             capacidad_equipo.fetchSumCapacidad(id_iteracion)
@@ -47,6 +48,137 @@ exports.postModificarHorasColaborador = (request, response) => {
 }
 
 
+exports.postModificarPorcentajeTiempoPerdido = (request, response) => {
+    const id_iteracion = request.session.idIteracion;
+    const id_capacidad = request.body.id_capacidad;
+    let tiempo_perdido_pc = request.body.tiempo_perdido_pc;
+
+    if(tiempo_perdido_pc > 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        tiempo_perdido_pc = (tiempo_perdido_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadTiempoPerdido(id_capacidad, tiempo_perdido_pc)
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                    response.redirect('capacidad-equipo');
+                } 
+                else {
+                    capacidad_equipo.callsetHorasProductivas(id_capacidad, id_iteracion)
+                    .then((fieldData)=> {
+                        response.redirect('capacidad-equipo');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
+exports.postModificarPorcentajeErroresRegistro = (request, response) => {
+    const id_iteracion = request.session.idIteracion;
+    const id_capacidad = request.body.id_capacidad;
+    let errores_registro_pc = request.body.errores_registro_pc;
+
+    if(errores_registro_pc > 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        errores_registro_pc = (errores_registro_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadErroresRegistro(id_capacidad, errores_registro_pc)
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                    response.redirect('capacidad-equipo');
+                } 
+                else {
+                    capacidad_equipo.callsetHorasProductivas(id_capacidad, id_iteracion)
+                    .then((fieldData)=> {
+                        response.redirect('capacidad-equipo');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
+
+exports.postModificarPorcentajeOverhead = (request, response) => {
+    const id_iteracion = request.session.idIteracion;
+    const id_capacidad = request.body.id_capacidad;
+    let overhead_pc = request.body.overhead_pc;
+
+    if(overhead_pc >= 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        overhead_pc = (overhead_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadOverhead(id_capacidad, overhead_pc)
+            .then(() => {
+                capacidad_equipo.callsetHorasProductivas(id_capacidad, id_iteracion)
+                    .then(()=> {
+                        response.redirect('capacidad-equipo');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
+exports.postModificarPorcentajeProductivas = (request, response) => {
+    const id_iteracion = request.session.idIteracion;
+    const id_capacidad = request.body.id_capacidad;
+    let productivas_pc = request.body.productivas_pc;
+
+    if(productivas_pc > 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        productivas_pc = (productivas_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadProductivas(id_capacidad, productivas_pc)
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                    response.redirect('capacidad-equipo');
+                } 
+                else {
+                    capacidad_equipo.callsetHorasProductivas(id_capacidad, id_iteracion)
+                    .then((fieldData)=> {
+                        response.redirect('capacidad-equipo');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
 exports.postModificarPorcentajeOperativos = (request, response) => {
     const id_capacidad = request.body.id_capacidad;
     let operativos_pc = request.body.operativos_pc;
@@ -58,7 +190,58 @@ exports.postModificarPorcentajeOperativos = (request, response) => {
     else{
         operativos_pc = (operativos_pc / 100).toFixed(2);
         capacidad_equipo.updateCapacidadOperativos(id_capacidad, operativos_pc)
-            .then(() => {
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                }
+                response.redirect('capacidad-equipo');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
+exports.postModificarPorcentajeHumano = (request, response) => {
+    const id_capacidad = request.body.id_capacidad;
+    let humano_pc = request.body.humano_pc;
+
+    if(humano_pc > 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        humano_pc = (humano_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadHumano(id_capacidad, humano_pc)
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                }
+                response.redirect('capacidad-equipo');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+}
+
+
+exports.postModificarPorcentajeCMMI = (request, response) => {
+    const id_capacidad = request.body.id_capacidad;
+    let cmmi_pc = request.body.cmmi_pc;
+
+    if(cmmi_pc > 100){
+        request.session.alerta = "Ingresa un porcentaje menor a 100%";
+        response.redirect('capacidad-equipo');
+    }
+    else{
+        cmmi_pc = (cmmi_pc / 100).toFixed(2);
+        capacidad_equipo.updateCapacidadCMMI(id_capacidad, cmmi_pc)
+            .then((fieldData) => {
+                if(fieldData[0].changedRows === 0){
+                    request.session.alerta = "El porcentaje ingresado sobrepasa el máximo posible o es el mismo.";
+                }
                 response.redirect('capacidad-equipo');
             })
             .catch(err => {
