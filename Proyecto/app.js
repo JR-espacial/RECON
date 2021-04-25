44bbb22be64b20fb2e9a86b45a663067e96f2925
Felipe <feliyepez2001@gmail.com>
2021-04-25 18:58:37 -0500

    Google Cloud, App Engine Deploy funcional

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -57,7 +57,8 @@ app.use('/proyectos', proyectos);
 app.use('/users', users);
 app.use('/home', home);
 app.use("/",(request, response,next) => {   
-    response.status(404).send('<html><head><meta charset="UTF-8"><title>Page not found</title></head><body><h1>Error 404</h1></body></html>')
+    // response.status(404).send('<html><head><meta charset="UTF-8"><title>Page not found</title></head><body><h1>Error 404</h1></body></html>')
+    response.status(404).redirect('/home');
 });
 
-app.listen(3000);
+app.listen(8080);
